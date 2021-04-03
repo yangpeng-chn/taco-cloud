61216f8334667a1a184abf20b8a0493154477e18
yangpeng-chn <yangpeng.chn@gmail.com>
2021-04-04 02:01:30 +0900

    add charpter 2

--- src/test/java/tacos/HomeControllerTest.java
+++ src/test/java/tacos/HomeControllerTest.java
@@ -11,8 +11,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.test.web.servlet.MockMvc;
+//import tacos.web.HomeController;
 
-@WebMvcTest(HomeController.class)   // <1>
+//@WebMvcTest(HomeController.class)   // <1>
 public class HomeControllerTest {
 
     @Autowired
